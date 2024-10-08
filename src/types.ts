c3761251eff8d09d19d8d0c74a9c27c97066cee8
harrisonp1 <paulharrisonwfc@gmail.com>
2024-10-08 08:26:09 +0100

    add basic data and types

---
+++ src/types.ts
@@ -0,0 +1,14 @@
+export interface Player {
+  id: string;
+  name: string;
+  div: number;
+}
+
+export interface Result {
+  id: string;
+  player1: string;
+  player2: string;
+  set1: string;
+  set2: string;
+  set3: string;
+}
